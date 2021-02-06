2fd815d0f87a7f23023e5aeced55341906a3f27a
Sisilia <sisilia770@gmail.com>
2021-02-06 01:18:17 -0700

    hosts working

--- gg_api.py
+++ gg_api.py
@@ -1,12 +1,39 @@
 '''Version 0.35'''
 
+import json
+import nltk
+import re
+from nltk.corpus import stopwords
+from textblob import TextBlob
+
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
-    # Your code here
+    with open('gg' + str(year) + '.json') as f:
+        data = json.load(f)
+    bigrams = []
+    stop_words = set(stopwords.words('english'))
+    stop_words.add("goldenglobes")
+
+    host_exp = re.compile('(host(s|ed|ing))')
+
+    for tweet in data:
+        tweet_text_words = tweet['text']
+        tweet_match = re.search(host_exp, tweet_text_words)
+        if tweet_match is not None:
+            curr_bigrams = nltk.bigrams(w for w in re.findall(r"[A-Z]+[a-z]+\b", tweet_text_words) if w.lower() not in stop_words)
+            for b in curr_bigrams:
+                bigrams.append(b)
+
+    most_freq = nltk.FreqDist(bigrams).most_common(2)
+    host1 = list(most_freq[0][0])
+    host2 = list(most_freq[1][0])
+    hosts = [host1[0] + " " + host1[1], host2[0] + " " + host2[1]]
+    print(hosts)
+
     return hosts
 
 def get_awards(year):
@@ -36,12 +63,19 @@ def get_presenters(year):
     # Your code here
     return presenters
 
+def get_sentiments(year):
+    with open('gg' + str(year) + '.json') as f:
+        data = json.load(f)
+    for tweet in data:
+        tweet_text = tweet['text'].lower()
+        blob = TextBlob(tweet_text)
+        tweet_sentiment = blob.sentences[0].sentiment.polarity
+
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
-    # Your code here
     print("Pre-ceremony processing complete.")
     return
 
@@ -51,8 +85,9 @@ def main():
     and then run gg_api.main(). This is the second thing the TA will
     run when grading. Do NOT change the name of this function or
     what it returns.'''
-    # Your code here
+    pre_ceremony()
     return
 
 if __name__ == '__main__':
-    main()
+    get_hosts(2013)
+    get_hosts(2015)
